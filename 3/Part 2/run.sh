f6c3566d3e1b72046f556edf89e27e6d05223d6b
Rutvij-1 <menavlikarrutvij@gmail.com>
2021-04-23 12:49:32 +0530

    Restored run script

--- 3/Part 2/run.sh
+++ 3/Part 2/run.sh
@@ -1,8 +1,8 @@
-policy_file_name=2019111032_2019111013d.policy
-model_file_name=2019111032_2019111013d.pomdp
-statistics_file_name=statistics_d.txt
-policy_gen_output=policy_gen_output_d.txt
-python3 part2.py 2019111013 4 $model_file_name > output_d.txt
+policy_file_name=2019111032_2019111013.policy
+model_file_name=2019111032_2019111013.pomdp
+statistics_file_name=statistics.txt
+policy_gen_output=policy_gen_output.txt
+python3 part2.py 2019111013 4 $model_file_name > output.txt
 cd sarsop/src/
 ./pomdpsol ../../$model_file_name -o ../../$policy_file_name > ../../$policy_gen_output
 ./pomdpeval ../../$model_file_name --policy-file ../../$policy_file_name --simLen 50 --simNum 500 > ../../temp.txt
